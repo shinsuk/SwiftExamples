2706645397f7dc8e63cad126a61cc39e1ce7b992
Bumseok <shinsuk@gwbs.net>
2019-01-14 17:17:36 +0900

    Add Drag Interaction to PDFView

--- PDFView1/PDFView1/ViewController.swift
+++ PDFView1/PDFView1/ViewController.swift
@@ -8,14 +8,13 @@
 import UIKit
 import PDFKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDragInteractionDelegate {
 
     @IBOutlet weak var pdfView: PDFView!
     @IBAction func copyAction(_ sender: Any) {
         UIGraphicsBeginImageContext(pdfView.bounds.size)
-        defer {
-            UIGraphicsEndImageContext()
-        }
+        defer { UIGraphicsEndImageContext() }
+
         if let context = UIGraphicsGetCurrentContext() {
             pdfView.layer.render(in: context)
             let image = UIGraphicsGetImageFromCurrentImageContext()
@@ -26,7 +25,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialize()
+        loadPdf()
+    }
 
+    func initialize() {
+        pdfView.isUserInteractionEnabled = true
+        pdfView.addInteraction(UIDragInteraction(delegate: self))
+    }
+
+    func loadPdf() {
         if let path = Bundle.main.path(forResource: "trisurf3d", ofType: "pdf") {
             let url = URL(fileURLWithPath: path)
             if let pdfDocument = PDFDocument(url: url) {
@@ -37,6 +45,21 @@ class ViewController: UIViewController {
         }
     }
 
+    func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+        if let imageView = interaction.view as? PDFView {
+            UIGraphicsBeginImageContext(pdfView.bounds.size)
+            defer { UIGraphicsEndImageContext() }
 
+            if let context = UIGraphicsGetCurrentContext() {
+                pdfView.layer.render(in: context)
+                guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return [] }
+                let provider = NSItemProvider(object: image)
+                let item = UIDragItem(itemProvider: provider)
+                return [item]
+            }
+        }
+
+        return []
+    }
 }
 
